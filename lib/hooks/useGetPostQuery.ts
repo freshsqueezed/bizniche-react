05c8cbee9f3de02d4bb3b5493f80a21dedf6c174
matthew-gordon <m.gordon5525@gmail.com>
2023-11-02 16:13:35 -0600

    Add useGetPostQuery

---
+++ lib/hooks/useGetPostQuery.ts
@@ -0,0 +1,40 @@
+import { QueryHookOptions, gql, useQuery } from "@apollo/client";
+import { BiznicheBlogPost } from "../types";
+
+interface GetPostQueryOptions {
+  input: {
+    id?: string;
+    slug?: string;
+  };
+}
+
+const GET_POST_QUERY = gql`
+  query GetPostQuery($input: GetPostQueryInput!) {
+    getPostQuery(input: $input) {
+      id
+      title
+      slug
+      description
+      author {
+        id
+        email
+        username
+        role
+      }
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export function useGetPostQuery(
+  queryOptions?: GetPostQueryOptions & QueryHookOptions
+) {
+  const { data, loading, error } = useQuery(GET_POST_QUERY, queryOptions);
+
+  return {
+    post: data?.getPostQuery as BiznicheBlogPost,
+    loading,
+    error,
+  };
+}
